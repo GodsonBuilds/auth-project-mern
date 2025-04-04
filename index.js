db5329e94fc0a1922793b9a04bfe2eb70542eb6e
Romuad-Zoungla <missimahouzoun@gmail.com>
2025-04-04 09:39:50 +0100

    update

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const  helmet  = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
-
+const mongoose = require('mongoose');
 app.use(cors());
 app.use(helmet())
 app.use(cookieParser())
 
+
+//Connexion à mongoose
+mongoose.connect(process.env.MONGO_URI).then( ()=>{
+    console.log('Connexion à la base de données réussie')
+}).catch(error => {
+    console.log(error)
+}
+)
 
 //comprendre que nous utilisons des données json
 app.use(express.json());
